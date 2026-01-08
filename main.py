756753ab06d3c8aa3b2b5b7545e81acb048e0510
Bilal <bilalkhamed1@gmail.com>
2026-01-08 12:32:38 +0300

    feat: use a dictioary for handling operations

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import questionary
-from src import categories, expenses, utils
+from src import categories, expenses, analysis, utils
 
 # Define file name
 DATA_FILE = 'data/categories.csv'
 
 operations = ['Add Category', 'Add Expense', 'View All Expenses', 'View By Category', 'View By Date' , 'Exit']
 
+operations = {
+    'Add Category': categories.add_category,
+    'Add Expense': expenses.add_expense,
+    'View All Expenses': expenses.view_expenses,
+    'View By Category': expenses.view_by_category,
+    'View By Date': lambda: expenses.view_by_date(utils.date_input()),
+    'View Analysis': analysis.main,
+    'Exit': lambda: print("Exiting the program.")
+}
 
 def main():
     while True:
         operation = questionary.select(
             "Select an operation:",
-            choices=operations
+            choices=operations.keys()
         ).ask()
 
-        if operation == 'Add Category':
-            categories.add_category()
-        elif operation == 'Add Expense':
-            expenses.add_expense()
-        elif operation == 'View All Expenses':
-            expenses.view_expenses()
-        elif operation == 'View By Category':
-            expenses.view_by_category()
-        elif operation == 'View By Date':
-            date = utils.date_input()
-            expenses.view_by_date(date)
-        elif operation == 'Exit':
-            print("Exiting the program.")
+        if operation == 'Exit':
+            operations[operation]()
             break
+        else:
+            operations[operation]()
+
+      
         
 if __name__ == "__main__":
     main()
